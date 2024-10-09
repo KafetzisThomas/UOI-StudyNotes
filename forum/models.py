a9031961f940e06e161ea2427597b1c3e5a296af
KafetzisThomas <tomkafetzis06@gmail.com>
2024-10-09 18:15:37 +0300

    Add topic selection for posts

--- forum/models.py
+++ forum/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+TOPICS = (
+    ("Software", "Software"),
+    ("Hardware", "Hardware"),
+    ("Operating Systems", "Operating Systems"),
+    ("Networking", "Networking"),
+)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    topic = models.CharField(max_length=100, choices=TOPICS, default=None)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
