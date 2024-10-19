e802a41d631c67854dd74694e628ab09390e6791
KafetzisThomas <tomkafetzis06@gmail.com>
2024-10-19 20:45:32 +0300

    Update PostForm fields to dark theme

--- forum/forms.py
+++ forum/forms.py
@@ -6,6 +6,22 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "topic", "content"]
+        widgets = {
+            "title": forms.Textarea(
+                attrs={"style": "background-color: #2c3035; color: #adb5bd;", "rows": 1}
+            ),
+            "topic": forms.Select(
+                attrs={"style": "background-color: #2c3035; color: #adb5bd;"}
+            ),
+            "content": forms.Textarea(
+                attrs={
+                    "class": "custom-textarea",
+                    "style": "background-color: #2c3035; color: #adb5bd;",
+                    "rows": 10,
+                    "placeholder": "Write your reply here...",
+                }
+            ),
+        }
 
 
 class ReplyForm(forms.ModelForm):
