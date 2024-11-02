37022c6b1e5d69e086f7630717dba1132fbd3334
KafetzisThomas <tomkafetzis06@gmail.com>
2024-11-02 21:49:20 +0200

    Import forgotten turnstile settings

--- main/settings.py
+++ main/settings.py
@@ -167,3 +167,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+# Turnstile settings (DEBUG=False)
+if not DEBUG:
+    TURNSTILE_SITEKEY = os.getenv("TURNSTILE_SITEKEY")
+    TURNSTILE_SECRET = os.getenv("TURNSTILE_SECRET")
