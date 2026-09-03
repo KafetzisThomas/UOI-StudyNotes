4ec989e6a0b3191614856e4a216fa74939e83ef9
KafetzisThomas <tomkafetzis06@gmail.com>
2026-09-03 14:54:43 +0300

    Add login_required decorator to account view

--- users/views.py
+++ users/views.py
@@ -1,56 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, CustomAuthenticationForm, UpdateUserForm
 from django.contrib.auth.views import LoginView
-from .utils import send_update_account_notification
 from django.contrib.auth.models import User
-from django.conf import settings
 from django.contrib import messages
-
+from .forms import CustomUserCreationForm, CustomAuthenticationForm, UpdateUserForm
 
 def register(request):
-    """
-    Register a new user.
-    """
     if request.method == "POST":
-        form = CustomUserCreationForm(data=request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("users:login")
     else:
         form = CustomUserCreationForm()
 
-    context = {"form": form}
-    return render(request, "users/register.html", context)
+    return render(request, "users/register.html", {"form": form})
 
-
+@login_required
 def account(request):
     if request.method == "POST":
         form = UpdateUserForm(instance=request.user, data=request.POST)
-        user = User.objects.get(id=request.user.id)
         if form.is_valid():
             form.save()
-            send_update_account_notification(user) if not settings.DEBUG else None
-            update_session_auth_hash(request, request.user)  # Keep user logged in
-            messages.success(
-                request, "Your account credentials have been successfully updated."
-            )
+            update_session_auth_hash(request, request.user)
+            messages.success(request, "Your account credentials have been successfully updated.")
             return redirect("users:account")
     else:
         form = UpdateUserForm(instance=request.user)
 
-    context = {"form": form}
-    return render(request, "users/account.html", context)
-
+    return render(request, "users/account.html", {"form": form})
 
 @login_required
 def delete_account(request):
     user = User.objects.get(id=request.user.id)
     user.delete()
-    messages.error(
-        request, "Your account has been deleted along with all associated data."
-    )
+    messages.error(request, "Your account has been deleted along with all associated data.")
     return redirect("users:register")
 
 
